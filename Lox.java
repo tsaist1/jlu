a2e93e4db4a654a1ee035a1b17de9aac9989e4aa
tsaist1 <gtxtzl@gmail.com>
2021-12-30 18:40:56 -0800

    add Lox.java

--- Lox.java
+++ Lox.java
@@ -8,6 +8,7 @@ import java.nio.file.Files;
 import java.util.List;
 
 public class Lox {
+    static boolean hadError = false;
     public static void main(String[] args) throws IOException {
 	if (args.length > 1) {
 	    System.out.println("Usage: jlox [script]");
