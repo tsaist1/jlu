53f5a8426f891f9a8b8e4d78a1cea3f72c2b3d5e
tsaist1 <gtxtzl@gmail.com>
2021-12-30 18:53:33 -0800

    add files

---
+++ TokenType.java
@@ -0,0 +1,20 @@
+package com.craftinginterpreters.lox;
+
+enum TokenType {
+    // single-char tokens
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // one or two char tokens
+    BANG, BANG_EQUAL, BANG, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
+
+    // literals
+    IDENTIFIER, STRING, NUMBER,
+
+    // keywords
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
+}
